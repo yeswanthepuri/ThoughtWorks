26ab69dacbce711cf598faebceaa76c2ba4851cc
Yeswanth^k <epuri.yeshu@gmail.com>
2020-02-24 20:15:15 +0530

    Update Event.cs

--- Program_Schedule/Model/Event/Event.cs
+++ Program_Schedule/Model/Event/Event.cs
@@ -5,67 +5,84 @@ using Program_Schedule.HelperClass;
 
 namespace Program_Schedule.Model
 {
-
     public class Event
     {
-        ITrack currentTrack = new Track();
-
-        Int16 trackCount = 0;
-
+        List<Track> tracks = new List<Track>();
+        private void AddTrack()
+        {
+            var morningSlot = new Track();
+            morningSlot.AddSlot(new MorningSlot());
+            tracks.Add(morningSlot);
+            AllocateItem.Item($"Track{TrackCount()}");
+        }
+        private ITrack GetTrack()
+        {
+            if (TrackCount() == 0)
+                AddTrack();
+            return tracks[tracks.Count() - 1];
+        }
+        private int TrackCount()
+        {
+            return tracks.Count();
+        }
+        //ITrack currentTrack = new Track();
         public void Schedule(List<ITalk> talksList)
         {
-            var slottimeLeft = false;
-            SlotType currentRunningSlot = new SlotType();
-            Slot currentSlot = new MorningSlot();
-            while (talksList.Any())
+            var slottimeLeft = true;
+            try
             {
-                if (currentTrack.GetSlotCount() == 2 && !slottimeLeft)
-                    currentTrack.ResetSlots();
-
-                if (currentTrack.GetSlotCount() == 0 && !slottimeLeft)
+                ITrack currentTrack = GetTrack();
+                Slot currentSlot = currentTrack.GetSlot();
+                while (talksList.Any())
                 {
-                    trackCount++;
-                    AllocateItem.Item($"Track{trackCount}");
-                    currentTrack.AddSlot(currentSlot);
-                    currentSlot = new MorningSlot();
-                    currentRunningSlot = SlotType.Morning;
-                }
-                else if (currentTrack.GetSlotCount() == 1 && !slottimeLeft)
-                {
-                    currentSlot = new EveningSlot();
-                    currentTrack.AddSlot(currentSlot);
-                    currentRunningSlot = SlotType.Evening;
-                }
-                //Assign Talks
-                foreach (var item in talksList)
-                {
-                    if (currentSlot.GetDuration() >= item.Duration && item.Duration > 5)
+                    if (!slottimeLeft)
                     {
-                        AllocateItem.Item($"{currentSlot.AllotedSlot()} {item.Title}");
-                        currentSlot.SlotDuration = currentSlot.AllotDurationforTalk(item.Duration);
-                        talksList.Remove(item);
-                        break;
+                        if (currentTrack.IsTrackFull())
+                            AddTrack();
+                        else
+                            currentTrack.AddSlot(new EveningSlot());
+
+                        currentTrack = GetTrack();
+                        currentSlot = currentTrack.GetSlot();
                     }
-                    else if (currentSlot.GetDuration() >= item.Duration &&
-                    currentRunningSlot == SlotType.Evening)
+
+                    //Assign Talks
+                    foreach (var item in talksList)
                     {
-                        AllocateItem.Item($"{currentSlot.AllotedSlot()} {item.Title}");
-                        currentSlot.SlotDuration = currentSlot.AllotDurationforTalk(item.Duration);
-                        talksList.Remove(item);
-                        break;
+                        if (currentSlot.GetDuration() >= item.Duration && item.Duration > 5)
+                        {
+                            AllocateItem.Item($"{currentSlot.AllotedSlot()} {item.Title}");
+                            currentSlot.SlotDuration = currentSlot.AllotDurationforTalk(item.Duration);
+                            talksList.Remove(item);
+                            break;
+                        }
+                        else if (currentSlot.GetDuration() >= item.Duration &&
+                        currentSlot.TypeOfSlot == SlotType.Evening)
+                        {
+                            AllocateItem.Item($"{currentSlot.AllotedSlot()} {item.Title}");
+                            currentSlot.SlotDuration = currentSlot.AllotDurationforTalk(item.Duration);
+                            talksList.Remove(item);
+                            break;
+                        }
+                    }
+
+                    slottimeLeft = (talksList.Count > 0 && currentSlot.GetDuration() > talksList.Min(x => x.Duration));
+
+                    if (currentSlot.TypeOfSlot == SlotType.Morning && !slottimeLeft)
+                    {
+                        AllocateItem.Item("Lunch");
+                    }
+                    else if (!slottimeLeft)
+                    {
+                        AllocateItem.Item($"{currentSlot.AllotedSlot()} Network");
+                        slottimeLeft = false;
                     }
                 }
 
-                slottimeLeft = (talksList.Count > 0 && currentSlot.GetDuration() > talksList.Min(x => x.Duration));
-
-                if (currentRunningSlot == SlotType.Morning && !slottimeLeft)
-                {
-                    AllocateItem.Item("Lunch");
-                }
-                else if (!slottimeLeft)
-                {
-                    AllocateItem.Item($"{currentSlot.AllotedSlot()} Network");
-                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
             }
         }
     }
