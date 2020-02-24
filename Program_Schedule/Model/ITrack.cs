bc7f3931d2daddc6215e8780bebec27148c52b78
Yeswanth^k <epuri.yeshu@gmail.com>
2020-02-24 20:14:22 +0530

    Update ITrack.cs

--- Program_Schedule/Model/ITrack.cs
+++ Program_Schedule/Model/ITrack.cs
@@ -7,7 +7,7 @@ namespace Program_Schedule.Model
     public interface ITrack
     {
         int NumberofSlots { get; set; }
-        Slot GetSlot(SlotType slotType);
+        Slot GetSlot();
         int GetSlotCount();
         void AddSlot(Slot slot);
         bool IsTrackFull();
