61d6e06f1ac67ff14cdb0c4456d83c00aac5fdfb
Yeswanth^k <epuri.yeshu@gmail.com>
2020-02-24 20:19:10 +0530

    Update CommonHelper.cs

--- Program_Schedule/HelperClasses/CommonHelper.cs
+++ Program_Schedule/HelperClasses/CommonHelper.cs
@@ -19,41 +19,6 @@ namespace Program_Schedule.HelperClass
             }
             return stringPath;
         }
-        //This Method is use to compute the Output
-        public static void Display(List<Talk> talks)
-        {
-            // var groupedTalks=talks.GroupBy(x => x.Day);
-            // try
-            // {
-            // foreach (var item in groupedTalks)
-            // {
-            //     System.Console.WriteLine($"Track{item.Key}");
-            //     var talksperdays = item.GroupBy(x => x.AssignedDuring);
-            //     foreach (var talkperday in talksperdays)
-            //     {
-            //         foreach (var talk in talkperday)
-            //         {
-            //             System.Console.WriteLine($"\t\t{talk.TimeDisplay} {talk.Title}");
-            //         }
-            //         if (talkperday.Key == SlotType.Morning)
-            //         {
-            //             System.Console.WriteLine($"\t\t12PM Lunch");
-            //         }
-            //         else
-            //         {
-            //             System.Console.WriteLine($"\t\t5PM Network Event");
-            //         }
-            //  }
-            // }
-            //}
-            //     catch(Exception ex)
-            //     {
-            //         ExceptionHandling.Excep = ex;
-            //          throw ExceptionHandling.Excep;
-            //     }
-            //    Console.ReadKey();
-        }
-
     }
 
 }
